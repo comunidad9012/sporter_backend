948e34294e60b9aaaaf9d29e63a434badbadaeab
Tincho25 <martinrodriguezbrignole@gmail.com>
2024-04-01 01:16:02 -0300

    4to commit?: probablemente 4to commit

--- product_api_service/api/__init__.py
+++ product_api_service/api/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 service_api = Blueprint("api", __name__, url_prefix="/api")
 
-from product_api_service.api.ejemplo import example_crud_bp as _example_crud_bp
+from product_api_service.api.api_update import updateProduct_bp as _example_crud_bp
 
 service_api.register_blueprint(_example_crud_bp)
