e0fd789bef4b28f2e8a7d681357052e34e292583
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-04-02 17:24:46 -0300

    fix: registrar endpoints de lectura en blueprint API

--- product_api_service/api/__init__.py
+++ product_api_service/api/__init__.py
@@ -5,3 +5,7 @@ service_api = Blueprint("api", __name__, url_prefix="/api")
 from product_api_service.api.altaProduct import altaProductBP as _altaProductBP
 
 service_api.register_blueprint(_altaProductBP)
+
+from product_api_service.api.producto_leer import product_read_bp as _product_read_bp
+
+service_api.register_blueprint(_product_read_bp)
