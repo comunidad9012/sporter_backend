70aa338f0aeafad95576b7c8dd393d59256ae954
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-04-01 23:34:12 -0300

    fix: logica de actualizacion en endpoint actualizar
    Sobrescribir imagen de producto previa, actualizar datos recibidos de usuario.

--- product_api_service/api/api_update.py
+++ product_api_service/api/api_update.py
@@ -1,13 +1,12 @@
 from typing import Dict
 from flask import Blueprint, jsonify, request
-from sqlalchemy import select, update, delete, or_
+from sqlalchemy import select, update
 from sqlalchemy.exc import IntegrityError
 from product_api_service.database.session import create_local_session
 from product_api_service import schemas, models
 from pydantic import ValidationError
-import os 
-from werkzeug.utils import secure_filename
-import uuid 
+import os
+
 
 updateProduct_bp = Blueprint(
     "updateProduct",
@@ -15,26 +14,31 @@ updateProduct_bp = Blueprint(
     url_prefix="/producto",
 )
 
+
 @updateProduct_bp.post("/actualizar")
 def update_existingProduct():
 
-    # Verificar si el nombre de la imagen ha sido enviado
-    if 'filename' in UpdateFiles:
-        imagen = UpdateFiles["filename"]
-        if not imagen.filename:  # Verificar si el nombre del archivo está vacío
-            return {"msj": "Nombre de archivo de imagen inválido"}, 400
-        
-    # Obtener los datos de la solicitud incluyendo los archivos
+    new_image: bool = False
+    # obtener de request: Datos e Imagen
     UpdateRequest: Dict = request.form.to_dict()
     UpdateFiles: Dict = request.files
+    # Si no hay imagen, tomar nota y no validar nombre de imagen, ya que no se modificara
+    if "filename" in UpdateFiles:
+        imagen = UpdateFiles["filename"]
+        new_image: bool = True
 
-    # Validar la solicitud incluyendo los archivos
+    if imagen.filename and new_image:  # Verificar si el nombre del archivo está vacío
+        UpdateRequest["img_orig_name"] = imagen.filename
+
+    # validar info
+
     try:
-        # Combinar datos de formulario y archivos en un solo diccionario
-        UpdateData = {**UpdateRequest, **UpdateFiles}
-
         # Validar utilizando el esquema de Producto
-        RequestValidada = schemas.Producto(**UpdateData).dict()
+        # Evitamos validar el nombre aleatorio, puesto que reutilizaremos
+        # el que esta en la DB para sobre escribir el archivo anterior
+        RequestValidada = dict(
+            schemas.Producto(**UpdateRequest).model_dump(exclude_defaults=True)
+        )
 
     except ValidationError as ve:
         errors_list = []
@@ -50,51 +54,49 @@ def update_existingProduct():
 
     try:
         with create_local_session() as db_session:
-            # Obtener la información del ejemplo antes de actualizar
-            old_example = (
-                db_session.query(models.Ejemplo)
-                .filter(models.Ejemplo.id == RequestValidada["id"])
-                .first()
+            # chequear que producto exista
+            find_product_query = select(models.Producto).where(
+                models.Producto.id == UpdateRequest["id"]
             )
 
-            # Con este se actualiza
-            update_data = {
-                "nombre": RequestValidada["nombre"],
-                "descripcion": RequestValidada["descripcion"],
-                "precio": RequestValidada["precio"],
-                "cantidad": RequestValidada["cantidad"],
-            }
+            product_to_update = db_session.execute(find_product_query).one_or_none()
 
-            # Verificar si hay una nueva imagen
-            if "filename" in UpdateRequest:
-                imagen = UpdateFiles["filename"]
-                # Generar un nombre único para la imagen
-                img_rand_name = str(uuid.uuid4()) + secure_filename(imagen.filename)
-                img_path = os.path.join(os.getenv('FILES_DIR'), img_rand_name)
-                imagen.save(img_path)
+            # si no, error
+            if product_to_update is None:
+                return {
+                    "msg": f"No se halló un producto con esta id {UpdateRequest['id']}"
+                }, 404
+            # si si:
+            product_to_update = product_to_update[0]
+            # Si no hay imagen, no cambiar cosas relacionadas a imagen
+            # Si hay imagen, obtener el nombre aleatorio original y sobre escribir
+            if new_image:
+                # sobre escribir
+                imagen.save(
+                    os.path.join(
+                        os.getenv("FILES_DIR"), product_to_update.img_rand_name
+                    )
+                )
 
-                # Eliminar la imagen anterior si existe
-                if old_example and old_example.img_rand_name:
-                    old_image_path = os.path.join(os.getenv('FILES_DIR'), old_example.img_rand_name)
-                    if os.path.exists(old_image_path):
-                        os.remove(old_image_path)
-
-                update_data["img_orig_name"] = imagen.filename
-                update_data["img_rand_name"] = img_rand_name
-
-            update_example_query: update = (
-                update(models.Ejemplo)
-                .where(models.Ejemplo.id == RequestValidada["id"])
-                .values(**update_data)
+            # Guardar nueva info de producto (id, nombre, descr, imag_orig, precio, exis)
+            update_product_query = (
+                update(models.Producto)
+                .where(models.Producto.id == product_to_update.id)
+                .values(**RequestValidada)
             )
-            db_session.execute(update_example_query)
+
+            db_session.execute(update_product_query)
             db_session.commit()
 
     except IntegrityError as ie:
         campo_repetido = ie.orig.args[1].split(".")[-1].strip("'")
-        return {"msj": f"El ejemplo que intento con {campo_repetido} crear ya existe "}, 400
+        return {
+            "msj": f"El ejemplo que intento con {campo_repetido} crear ya existe "
+        }, 400
     except Exception as e:
         db_session.rollback()
         return {"msj": "Error interno del servidor"}, 500
 
-    return {"msj": f"Ejemplo {RequestValidada['nombre']} actualizado exitosamente"}, 200
+    return {
+        "msg": f"Producto {product_to_update.id} - {product_to_update.nombre} actualizado exitosamente"
+    }, 200
