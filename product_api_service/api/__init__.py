69b3b232b33e6532e81270abff9666169a6c4848
unknown <ramirezyhonny0@gmail.com>
2024-04-01 22:29:31 -0300

    cambios_rama_eliminar

--- product_api_service/api/__init__.py
+++ product_api_service/api/__init__.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+# Crear un blueprint
 service_api = Blueprint("api", __name__, url_prefix="/api")
 
 from product_api_service.api.altaProduct import altaProductBP as _altaProductBP
@@ -13,3 +14,7 @@ service_api.register_blueprint(_product_read_bp)
 from product_api_service.api.api_update import updateProduct_bp as _updateProduct_bp
 
 service_api.register_blueprint(_updateProduct_bp)
+
+from product_api_service.api.blueprint_elim import blueprint_eliminar
+
+service_api.register_blueprint(blueprint_eliminar)
