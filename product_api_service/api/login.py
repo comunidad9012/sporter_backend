c29860812409020ca5c90dfbd4bc63aaa1880239
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-04-29 02:57:28 -0300

    feat: agregar registro de inicio de sesion en login

--- product_api_service/api/login.py
+++ product_api_service/api/login.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, make_response
 import bcrypt
 #import jwt
-from product_api_service.models import User
+from product_api_service.models import User, Sesion
 from product_api_service.database.session import create_local_session
 
 login_bp=Blueprint("login",__name__,url_prefix="/user")
@@ -16,6 +16,12 @@ def login():
                 respuesta=make_response(jsonify({"mensaje":"Inicio de sesión exitosa"}))
                 respuesta.set_cookie("logged_in","1",max_age=3600)
                 respuesta.set_cookie("is_admin", str(int(user.is_admin)), max_age=3600)
+
+                tiempo_inicio_sesion = int(request.headers.environ["HTTP_STARTED"][:-3])
+                registro_sesion = Sesion(id_usuario = user.id, login_time_unix=tiempo_inicio_sesion)
+                db.add(registro_sesion)
+                db.commit()
+
                 return respuesta, 200  
             else:
                 respuesta=make_response(jsonify({"error":"Credenciales inválidas"}),401)
