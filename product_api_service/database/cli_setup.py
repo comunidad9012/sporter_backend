d09bc031868b5e957da7de7af66fae2f94d7ba4b
German Rinaldi <110374480+ger-rinaldi@users.noreply.github.com>
2024-05-01 01:39:43 -0300

    chore: agregar creacion de admin al crear tablas

--- product_api_service/database/cli_setup.py
+++ product_api_service/database/cli_setup.py
@@ -1,11 +1,13 @@
-from sqlalchemy import text
-from sqlalchemy.orm import Session
+import bcrypt
 from flask.cli import AppGroup
+from sqlalchemy import text
+from sqlalchemy.orm import Session, sessionmaker
+
 from product_api_service import models
 from product_api_service.database.custom_declarative_base import CustomBase
 from product_api_service.database.engine import (
-    ENGINE,
     DB_CREDENTIALS,
+    ENGINE,
     URL,
     create_engine,
 )
@@ -17,6 +19,7 @@ db_setup: AppGroup = AppGroup("db-cli")
 def create_schemas(test_db: bool = False):
     """Crear las tablas de la DB"""
     _orm_create_models()
+    _orm_creat_admin()
 
 
 @db_setup.command("crear-db")
@@ -30,6 +33,25 @@ def create_everything(test_db: bool = False):
     """Crea la DB y las TABLAS"""
     _orm_create_db()
     _orm_create_models()
+
+
+def _orm_creat_admin():
+    session_local = sessionmaker(autoflush=False, autocommit=False, bind=ENGINE)
+
+    with session_local() as sesion:
+        passwd = bcrypt.hashpw("1234567890".encode("UTF-8"), bcrypt.gensalt())
+
+        usuario_admin = models.User(
+            nombre="admin",
+            usuario="admin",
+            correo="admin@example.com",
+            is_admin=1,
+            contraseña=passwd,
+        )
+
+        sesion.add(usuario_admin)
+
+        sesion.commit()
 
 
 def _orm_create_models():
